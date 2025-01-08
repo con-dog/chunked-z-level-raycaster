962a203c7894395d3ae890cba8de35e9c035adc8
Papillon <75226598+con-dog@users.noreply.github.com>
2025-01-08 15:26:52 +1300

    lava assets

---
+++ assets/textures/lava/lava-a/lava-a.h
@@ -0,0 +1,8 @@
+#ifndef LAVA_A_H
+#define LAVA_A_H
+
+#include "../../types.h"
+
+extern Pixel_Image_Asset lava_a;
+
+#endif
---
+++ assets/textures/lava/lava-b/lava-b.h
@@ -0,0 +1,8 @@
+#ifndef LAVA_B_H
+#define LAVA_B_H
+
+#include "../../types.h"
+
+extern Pixel_Image_Asset lava_b;
+
+#endif
